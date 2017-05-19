a936ab28b5866ac65a2698aa051cc39a1edb19c5
Arthus <531151995@qq.com>
2017-05-19 09:59:42 +0800

    update list

--- js/list.js
+++ js/list.js
@@ -15,11 +15,29 @@ List.prototype={
 		me.BtnThumbnail.bind('click',function(){
 			me.switchClass(me.Con[0],'article_con_thumbnail');
 		});
+		me.initClass();
 	},
 	switchClass:function(dom,className){
 		if(dom.className !== className){
 			dom.className = className;
+			this.localData('listView',className);
 		}
+	},
+	initClass:function(){
+		var _val = this.localData('listView');
+		if(_val){
+			this.switchClass(this.Con[0],_val);
+		}
+	},
+	localData:function(key,value){
+		if(localStorage && localStorage.setItem){
+			if(value!==undefined){
+				return localStorage.setItem(key,value);
+			}else{
+				return localStorage.getItem(key);
+			}
+		}
+		return none;
 	}
 };
 
