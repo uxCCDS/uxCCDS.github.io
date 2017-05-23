1f9d8aa0d38ce08986fb976987d3f9545b6e9bef
Arthus <531151995@qq.com>
2017-05-23 13:30:57 +0800

    update team map

--- js/team.js
+++ js/team.js
@@ -1,9 +1,27 @@
 (function(){
 	var DOC,WIN;
 
+	var MapItem = function(dom){
+		this.Dom = $(dom);
+		this.DomFloat = $('#tf_'+this.Dom .attr('data-tf'));
+
+		this.init();
+	};
+	MapItem.prototype={
+		init:function(){
+			var me = this;
+			this.Dom.bind('mouseenter',function(){
+				me.DomFloat.css('display','');
+			});
+			this.Dom.bind('mouseleave',function(){
+				me.DomFloat.css('display','none');
+			});
+		}
+	};
+
 	var WorldMap = function(){
 		this.Dom = $('#svg_map');
-	
+		this.Items = $('[data-tf]');
 		this.init();
 	};
 	WorldMap.prototype={
@@ -13,6 +31,13 @@
 			WIN.bind('resize',function(){
 				me.resize();
 			});
+
+			me.initItems();
+		},
+		initItems:function(){
+			for(var i=0,l=this.Items.length;i<l;i++){
+				new MapItem(this.Items[i]);
+			}
 		},
 		resize:function(){
 			var w = this.Dom.width(),
